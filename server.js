b961833c1e0acf9fb83d6c783f3712399aaf00db
henrymcbean <henry_mcbean@hotmail.com>
2021-04-23 23:26:37 +0100

    008 Route Files With Express Router.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express(express.json());
 
 // Db Config
 const db = require('./config/keys').mongoURI;
@@ -13,6 +17,11 @@ mongoose
   .catch(errr => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
