0da1e5e93362fa90f60a4d24e785f2d7d898fa66
henrymcbean <henry_mcbean@hotmail.com>
2021-04-23 22:09:16 +0100

    007 Connecting To MongoDB With Mongoose.

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://HenryMcBean:Try14RtOPwed@cluster0-m9ynr.mongodb.net/devconnector?retryWrites=true&w=majority'
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Db Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(errr => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
